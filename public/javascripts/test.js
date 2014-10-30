66340013495dd7ab47a79ed35584dfb6f439bf63
長廻達也 <nagasakotatsuya@nagamawatatsuya-no-iMac.local>
2014-10-30 23:24:16 +0900

    アニメーションが完了してから勝敗判定するよう修正

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
     var $img_batsu = $("#img-batsu").clone().removeAttr("id");
     //$("#box5").append($img_maru.clone());
 
+    var cnt = 0;
+
     //○×を回転させる処理
     function rotateMaruBatsu($obj) {
         $obj
@@ -18,7 +20,8 @@ $(document).ready(function() {
                     $(this).css("opacity", now);
                 },
                 complete: function() {
-                    $(this).css("rotationCounter", 0);
+                    //勝ち負け判定
+                    checkWinner();
                 }
             }
         );
@@ -35,93 +38,92 @@ $(document).ready(function() {
     $("#clear").on("click", function() {
         location.reload();
     });
+    setBoxEvent();
 
-    var cnt = 0;
+    //全部のマスのイベントを解除する
+    function removeBoxEvent() {
+        for (var k=1; k<=9; ++k) {
+            $("#box" + k).off();
+        }
+    }
 
-    for (var i=1; i<=9; ++i) {
-        (function(i) {
-            $("#box" + i).on("click", function() {
-                if ($(this).attr("checkmark") != undefined) {
-                    alert("すでに選択済みだ馬鹿野郎");
-                } else {
-                    cnt = cnt + 1;
-                    if (cnt % 2 == 0) {
-                      $(this).attr("checkmark", "x");
-                      var $appendImage = $img_batsu.clone();
-                      rotateMaruBatsu($appendImage);
-                      $(this).append($appendImage);
-                  } else { 
-                    $(this).attr("checkmark", "o");
-                    var $appendImage = $img_maru.clone();
-                      rotateMaruBatsu($appendImage);
-                      $(this).append($appendImage);
-                    //$(this).append($img_maru.clone());
-                  }
-                }
-                //揃っているかどうかの判定処理
-                //勝ち負け・・・入力のたびにチェック
-                
-                var isWinnerExist = false;
-
-                for (var j=1; j<=7; j+=3) {
-                    if ($("#box" + j).attr("checkmark") != undefined) {
-                        if ($("#box" + j).attr("checkmark") == $("#box" + (j+1)).attr("checkmark") && $("#box" + j).attr("checkmark")==$("#box" + (j+2)).attr("checkmark") ) 
-                        {
-                            alert($("#box" + j).attr("checkmark") + "の勝ち");
-                            isWinnerExist = true;
-                            for (var k=1; k<=9; ++k) {
-                                $("#box" + k).off();
-                            }
-                        }
+    function setBoxEvent() {
+        for (var i=1; i<=9; ++i) {
+            (function(i) {
+                $("#box" + i).on("click", function() {
+                    removeBoxEvent(); //クリックされたらイベントを解除
+                    if ($(this).attr("checkmark") != undefined) {
+                        alert("すでに選択済みだ馬鹿野郎");
+                    } else {
+                        cnt = cnt + 1;
+                        if (cnt % 2 == 0) {
+                          $(this).attr("checkmark", "x");
+                          var $appendImage = $img_batsu.clone();
+                          rotateMaruBatsu($appendImage);
+                          $(this).append($appendImage);
+                      } else { 
+                        $(this).attr("checkmark", "o");
+                        var $appendImage = $img_maru.clone();
+                          rotateMaruBatsu($appendImage);
+                          $(this).append($appendImage);
+                        //$(this).append($img_maru.clone());
+                      }
                     }
-                }
-                for (var j=1; j<=3; ++j) {
-                    if ($("#box" + j).attr("checkmark") != undefined) {
-                        if ($("#box" + j).attr("checkmark") == $("#box" + (j+3)).attr("checkmark") && $("#box" + j).attr("checkmark") == $("#box" + (j+6)).attr("checkmark"))
-                        {
-                            alert($("#box" + j).attr("checkmark") + "の勝ち！");
-                            isWinnerExist = true;
-                            for (var k=1; k<=9; ++k) {
-                                $("#box" + k).off();
-                            }
-                        }
-                    }
-                }
-                if ($("#box1").attr("checkmark") != undefined)
+                });
+            })(i);
+        }
+    }
+    //勝ち負け判定
+    function checkWinner() {
+        var isWinnerExist = false;
+        for (var j=1; j<=7; j+=3) {
+            if ($("#box" + j).attr("checkmark") != undefined) {
+                if ($("#box" + j).attr("checkmark") == $("#box" + (j+1)).attr("checkmark") && $("#box" + j).attr("checkmark")==$("#box" + (j+2)).attr("checkmark") )
                 {
-                    if ($("#box1").attr("checkmark") == $("#box5").attr("checkmark") && 
-                        $("#box1").attr("checkmark") == $("#box9").attr("checkmark")) 
-                    {
-                        alert($("#box" + 1).attr("checkmark") + "の勝ち");
-                        isWinnerExist = true;
-                        for (var k=1; k<=9; ++k) {
-                            $("#box" + k).off();
-                        }
-                    }
+                    alert($("#box" + j).attr("checkmark") + "の勝ち");
+                    isWinnerExist = true;
+                    removeBoxEvent();
                 }
-                if ($("#box3").attr("checkmark") != undefined) 
+            }
+        }
+        for (var j=1; j<=3; ++j) {
+            if ($("#box" + j).attr("checkmark") != undefined) {
+                if ($("#box" + j).attr("checkmark") == $("#box" + (j+3)).attr("checkmark") && $("#box" + j).attr("checkmark") == $("#box" + (j+6)).attr("checkmark"))
                 {
-                    if ($("#box3").attr("checkmark") == $("#box5").attr("checkmark") && 
-                        $("#box3").attr("checkmark") == $("#box7").attr("checkmark")) 
-                    {
-                        alert($("#box" + 3).attr("checkmark") + "の勝ち");
-                        isWinnerExist = true;
-                        for (var k=1; k<=9; ++k) {
-                            $("#box" + k).off();
-                        }
-                    }
+                    alert($("#box" + j).attr("checkmark") + "の勝ち！");
+                    isWinnerExist = true;
+                    removeBoxEvent();
                 }
-                //引き分け・・・cnt = 9のとき
-                if (cnt == 9 && isWinnerExist == false)
-                {
-                    alert("引き分け");
-                    for (var k=1; k<=9; ++k) {
-                        $("#box" + k).off();
-                    }
-                }
-
-            });
-        })(i);
+            }
+        }
+        if ($("#box1").attr("checkmark") != undefined)
+        {
+            if ($("#box1").attr("checkmark") == $("#box5").attr("checkmark") &&
+                $("#box1").attr("checkmark") == $("#box9").attr("checkmark"))
+            {
+                alert($("#box" + 1).attr("checkmark") + "の勝ち");
+                isWinnerExist = true;
+                removeBoxEvent();
+            }
+        }
+        if ($("#box3").attr("checkmark") != undefined)
+        {
+            if ($("#box3").attr("checkmark") == $("#box5").attr("checkmark") &&
+                $("#box3").attr("checkmark") == $("#box7").attr("checkmark"))
+            {
+                alert($("#box" + 3).attr("checkmark") + "の勝ち");
+                isWinnerExist = true;
+                removeBoxEvent();
+            }
+        }
+        //引き分け・・・cnt = 9のとき
+        if (cnt == 9 && isWinnerExist == false)
+        {
+            alert("引き分け");
+            removeBoxEvent();
+        } else if(isWinnerExist == false) {
+            setBoxEvent();
+        }
     }
 });
 
