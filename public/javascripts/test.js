a4a70aa23ac2f8968577945df72c0eb08bb9ff1c
himawarigumi <ytxbr794@yahoo.co.jp>
2014-11-05 01:34:46 +0900

    ネット対戦

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -7,6 +7,11 @@ $(document).ready(function() {
 
     var cnt = 0;
 
+    var myTurn = window.turn; // 先攻or後攻
+    $("#title").text($("#title").text() + " (" + ((myTurn == 0) ? "先攻" : "後攻") + ") ")
+    var prevStatus = JSON.parse($.ajax({ url: "/get", async: false }).responseText);
+
+
     //○×を回転させる処理
     function rotateMaruBatsu($obj) {
         $obj
@@ -20,8 +25,22 @@ $(document).ready(function() {
                     $(this).css("opacity", now);
                 },
                 complete: function() {
-                    //勝ち負け判定
+                    //勝ち負け判定(旧)
                     checkWinner();
+                    //勝ち負け判定(新)
+                    // var result = getWinner();
+                    // if (result == true) {
+                    //     //勝ち判定
+                    //     //alert($(this).attr("checkmark") + "の勝ち");
+                    //     alert("maru")
+                    //     removeBoxEvent();
+                    // } else if (cnt == 9) {
+                    //     //引き分け判定
+                    //     alert("引き分け");
+                    //     removeBoxEvent();
+                    // } else {
+                    //     setBoxEvent();
+                    // }
                 }
             }
         );
@@ -35,10 +54,9 @@ $(document).ready(function() {
 
     // }
 
-    $("#clear").on("click", function() {
-        location.reload();
-    });
-    setBoxEvent();
+    // $("#clear").on("click", function() {
+    //     location.reload();
+    // });
 
     //全部のマスのイベントを解除する
     function removeBoxEvent() {
@@ -50,30 +68,39 @@ $(document).ready(function() {
     function setBoxEvent() {
         for (var i=1; i<=9; ++i) {
             (function(i) {
-                $("#box" + i).on("click", function() {
-                    removeBoxEvent(); //クリックされたらイベントを解除
-                    if ($(this).attr("checkmark") != undefined) {
-                        alert("すでに選択済みだ馬鹿野郎");
-                    } else {
-                        cnt = cnt + 1;
-                        if (cnt % 2 == 0) {
-                          $(this).attr("checkmark", "x");
-                          var $appendImage = $img_batsu.clone();
-                          rotateMaruBatsu($appendImage);
-                          $(this).append($appendImage);
-                      } else { 
-                        $(this).attr("checkmark", "o");
-                        var $appendImage = $img_maru.clone();
-                          rotateMaruBatsu($appendImage);
-                          $(this).append($appendImage);
-                        //$(this).append($img_maru.clone());
-                      }
+                $("#box" + i).on("click rivalClick", function(e) {
+                    if (cnt % 2 == myTurn || e["type"] == "rivalClick") {
+                        removeBoxEvent(); //クリックされたらイベントを解除
+                        if ($(this).attr("checkmark") != undefined) {
+                            alert("すでに選択済みだ馬鹿野郎");
+                            setBoxEvent();
+                        } else {
+                            cnt = cnt + 1;
+                            if (cnt % 2 == 0) {
+                              $(this).attr("checkmark", "x");
+                              var $appendImage = $img_batsu.clone();
+                              rotateMaruBatsu($appendImage);
+                              $(this).append($appendImage);
+                            } else { 
+                            $(this).attr("checkmark", "o");
+                            var $appendImage = $img_maru.clone();
+                              rotateMaruBatsu($appendImage);
+                              $(this).append($appendImage);
+                            //$(this).append($img_maru.clone());
+                            }
+                            if (e["type"] == "click") {
+                                var x = ((i - 1) / 3) | 0;
+                                var y = (i - 1) % 3;
+                                $.ajax({ url: "/put/" + x + "/" + y + "/" + (myTurn + 1), async: false });
+                                prevStatus[i - 1] = 1;
+                            }
+                        }
                     }
                 });
             })(i);
         }
     }
-    //勝ち負け判定
+    //勝ち負け判定(旧)
     function checkWinner() {
         var isWinnerExist = false;
         for (var j=1; j<=7; j+=3) {
@@ -125,6 +152,81 @@ $(document).ready(function() {
             setBoxEvent();
         }
     }
+
+    //勝ち負け判定(新)
+    function getWinner() {
+        //横に揃っているかどうかを走査
+        var isWinner = true;
+        for (var j=1; j<=7; j+=3) {
+            if ($("#box" + j).attr("checkmark") != undefined) { //空白かどうかの判定
+                if ($("#box" + j).attr("checkmark") == $("box" + (j+1)).attr("checkmark") &&
+                    $("#box" + j).attr("checkmark") == $("#box" + (j+2)).attr("checkmark"))
+                {
+                    return isWinner;
+                }
+            }
+        }
+        //縦に揃っているかどうかを走査
+        for (var j=1; j<=3; ++j) {
+            if ($("#box" + j).attr("checkmark") != undefined) {
+                if ($("#box" + j).attr("checkmark") == $("#box" + (j+3)).attr("checkmark") &&
+                    $("#box" + j).attr("checkmark") == $("#box" + (j+7)).attr("checkmark"))
+                {
+                    return isWinner;
+                }
+            }
+        }
+        //斜めが揃っているかどうかを走査(右下がり)
+        if ($("#box1").attr("checkmark") != undefined)
+        {
+            if ($("#box1").attr("checkmark") == $("#box5").attr("checkmark") &&
+                $("#box1").attr("checkmark") == $("#box9").attr("checkmark"))
+            {
+                return isWinner;
+            }
+        }
+        //斜めが揃っているかどうかを走査(左下がり)
+        if ($("#box3").attr("checkmark") != undefined)
+        {
+            if ($("#box3").attr("checkmark") == $("#box5").attr("checkmark") &&
+                $("#box3").attr("checkmark") == $("#box9").attr("checkmark"))
+            {
+                return isWinner;
+            }
+        }
+    }
+
+    //$.ajax({ url: "/put/" + x + "/" + y + "/" + (myTurn + 1), async: false });
+    //JSON.parse($.ajax({ url: "/get", async: false }).responseText);
+    //$.ajax({ url: "/reset", async: false });
+    //XMLHttpRequest()
+    //var jsonData = JSON.parse($.ajax({ url: "/get", async: false }).responseText);
+
+
+    //クリアボタンでリロード
+    $("#clear").on("click", function() {
+        $.ajax({ url: "/reset", async: false });
+        location.reload();
+    });
+
+    //マスにイベント割り当て
+    setBoxEvent();
+
+        // ポーリング
+    setInterval(function() {
+        var status = JSON.parse($.ajax({ url: "/get", async: false }).responseText);
+        for (var i=0; i<9; i++) {
+            if (status[i] !== prevStatus[i]) {
+                if ($("#box" + (i + 1)).attr("checkmark") == undefined) {
+                    $("#box" + (i + 1)).trigger("rivalClick");
+                }
+                break;
+            }
+        }
+        prevStatus = status;
+    }, 1000);
+
+
 });
 
 
